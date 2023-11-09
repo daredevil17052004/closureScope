056424fe781fa4d5cb062315589d79714977bf71
anshs <anshs052004@gmail.com>
2023-11-08 17:15:07 -0800

    final commit

--- src/app.js
+++ src/app.js
@@ -11,3 +11,28 @@
 const arrayOfNumbers = [1,2,3,4,5,6];
 const itemToSearch = 4;
 
+const NEG_INF = -1000000;
+
+function createPop(arrayOfNumbers,itemToSearch){
+  let currIndex = NEG_INF;
+  let check = true;
+  
+  function searchForElement(){
+    for(let i =0; i<arrayOfNumbers.length; i++){
+      if(arrayOfNumbers[i]===itemToSearch){
+        currIndex = i;
+        check = false;
+        break;
+      }
+    }
+  }
+    searchForElement();
+      if(check === false){
+        return `The item is present and is at index ${currIndex}`;
+      } else{
+        return `The item is not present and is at index -1000000`;
+      }
+    }
+
+
+createPop(arrayOfNumbers,itemToSearch);
